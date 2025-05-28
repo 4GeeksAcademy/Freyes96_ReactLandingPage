dcf8a2944077f3df947068ba0906781aee65343e
freyes96 <177784317+freyes96@users.noreply.github.com>
2025-05-28 11:20:12 +0000

    Second commit

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ image, title, text, link, buttonLabel }) => {
     return (
         <div className="col-lg-3 col-md-6 d-flex justify-content-center mb-5">
-            <div className="card" style={{ width: "500px", height: "325px" }}>
+            <div className="card" style={{ width: "500px", height: "400px" }}>
                 <img src={image} className="card-img-top" alt={title} style={{ height: "60%", objectFit: "cover" }} />
                 <div className="card-body d-flex flex-column justify-content-between">
                     <h5 className="card-title">{title}</h5>
